81c048b62ee560412bb9fd4f6d69ed8d4f698397
hashan dharmapriya <hashan@i4tlabs.io>
2023-10-25 03:00:12 +0530

    fixes

--- src/navigation/screens/main/HomeScreen.tsx
+++ src/navigation/screens/main/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React, {memo, useEffect} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, SafeAreaView, SectionList, StatusBar, StyleSheet, Text, View } from "react-native";
 import {FONTS} from '../../../constants/fonts';
 import Icon from 'react-native-vector-icons/Feather';
 import SplashScreen from 'react-native-splash-screen';
 import {connect} from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const mapProps = (state: {state: any}) => {
   console.log(state);
@@ -19,17 +20,71 @@ const HomeScreen = (): JSX.Element => {
       SplashScreen?.hide();
     }
   }, []);
+
+  const DATA = [
+    {
+      title: 'Main dishes',
+      data: ['Pizza', 'Burger', 'Risotto'],
+    },
+    {
+      title: 'Sides',
+      data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+    },
+    {
+      title: 'Drinks',
+      data: ['Water', 'Coke', 'Beer'],
+    },
+    {
+      title: 'Desserts',
+      data: ['Cheese Cake', 'Ice Cream'],
+    },
+  ];
+
   return (
-    <View style={styles.dummy}>
+    <SafeAreaProvider>
+      <SafeAreaView>
       <Icon name="camera" size={30} color="#900" />
       <Text style={{fontFamily: FONTS.BOLD, fontSize: 100}}>helloa</Text>
-    </View>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({section: {title}}) => (
+          // <Text style={styles.header}>{title}</Text>
+          <></>
+        )}
+
+
+      />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   dummy: {
     backgroundColor: 'red',
+  },
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
   },
 });
 
--- src/navigation/stacks/SecuredStack.tsx
+++ src/navigation/stacks/SecuredStack.tsx
@@ -2,17 +2,18 @@ import {createStackNavigator} from '@react-navigation/stack';
 import BottomNavigation from './BottomNavigation';
 import React, {JSX} from 'react';
 import {SCREEN_NAMES} from '../../constants/screens';
+import HomeScreen from "../screens/main/HomeScreen";
 
 const Stack = createStackNavigator();
 
 const SecuredStack = (): JSX.Element => {
   return (
     <Stack.Navigator
-      initialRouteName={SCREEN_NAMES.BOTTOM_NAV}
+      initialRouteName={SCREEN_NAMES.HOME_SCREEN}
       screenOptions={{headerShown: false}}>
       <Stack.Screen
-        name={SCREEN_NAMES.BOTTOM_NAV}
-        component={BottomNavigation}
+        name={SCREEN_NAMES.HOME_SCREEN}
+        component={HomeScreen}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
