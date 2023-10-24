af4ed652bb3bc6c57f6375feeb7f92e4d50f8a8f
hashan dharmapriya <hashan@i4tlabs.io>
2023-10-25 01:05:17 +0530

    config android : done

--- src/navigation/screens/main/HomeScreen.tsx
+++ src/navigation/screens/main/HomeScreen.tsx
@@ -1,22 +1,19 @@
-import React, { useEffect } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, {useEffect} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import {FONTS} from '../../../constants/fonts';
 import Icon from 'react-native-vector-icons/Feather';
-import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from 'react-native-splash-screen';
 
 const App = (): JSX.Element => {
-  useEffect(()=> {
-    if (Platform.OS === 'android'){
-
+  useEffect(() => {
+    if (Platform.OS === 'android') {
       SplashScreen?.hide();
     }
-  },[])
+  }, []);
   return (
     <View style={styles.dummy}>
       <Icon name="camera" size={30} color="#900" />
-      <Text style={{fontFamily: FONTS.BOLD, fontSize: 100}}>
-        helloa
-      </Text>
+      <Text style={{fontFamily: FONTS.BOLD, fontSize: 100}}>helloa</Text>
     </View>
   );
 };
