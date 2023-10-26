41260e085482a585f4dafd6a8bd1584444eb3cc4
hexhad <hexhad@gmail.com>
2023-10-26 16:11:36 +0530

    header of the flat list and custom section scroller

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+export const IMAGES = {
+    HEADER_LOGO:require('./logo-teal.png'),
+    EAT_ME_LOGO:require('./img.png'),
+    HERO_COVER_IMAGE:require('./img_1.png'),
+
+    METHOD_DELEVER: require('./devliver.png')
+}
